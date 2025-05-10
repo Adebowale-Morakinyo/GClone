dc6b651a24ea946d15be5003fb20a91eef39104a
Adebowale Morakinyo <adebowalemorakinyo@gmail.com>
2025-05-10 20:25:23 +0100

    refactor(main): refactor parse logic to a Config method & enhance error handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,14 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect(); 
 
-    let config: Config = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguements: {}", err);
+        process::exit(1)
+    });
 
     println!("Searching for {}", config.query);
     println!("In File {}", config.filename);
@@ -20,9 +24,15 @@ struct Config {
     filename: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query: String = args[1].clone();
-    let filename: String = args[2].clone();
-    
-    Config { query, filename }
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query: String = args[1].clone();
+        let filename: String = args[2].clone();
+        
+        Ok(Config { query, filename })
+    }
 }
