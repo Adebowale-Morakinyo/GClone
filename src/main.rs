c134a26db2d7445df4c1bd9cbcb59c7dfff85095
Adebowale Morakinyo <adebowalemorakinyo@gmail.com>
2025-05-10 19:59:03 +0100

    feat(main): add basic implementation & args parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect(); 
+
+    let query: &String = &args[1];
+    let filename: &String = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In File {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents)
 }
