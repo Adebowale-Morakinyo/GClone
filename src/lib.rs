dcf962e1baf031e5006beac26b1c53a1d254aaa8
Adebowale Morakinyo <adebowalemorakinyo@gmail.com>
2025-05-20 08:38:03 +0100

    feat(TDD): implement search function following TDD

--- src/lib.rs
+++ src/lib.rs
@@ -32,3 +32,33 @@ impl Config {
         Ok(Config { query, filename })
     }
 }
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results: Vec<&str> = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query: &str = "duct";
+        let contents: &str = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+}
